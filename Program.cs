d118d10e7cc7262b874b8feb564f45705295fd83
GhostrykerII <tyler_kasper@hotmail.com>
2022-10-31 10:42:31 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loops");
+            Console.ReadKey(true);
+        }
+    }
+}
